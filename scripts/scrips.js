e1e93fca87ea259fc2f40402deeafe3937b8581e
Yesenia <hernandezy1603@gmail.com>
2019-05-20 15:56:11 -0500

    al cien

---
+++ scripts/scrips.js
@@ -0,0 +1,25 @@
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+		function firmado(event){
+            //alert(event.keyCode);
+            if(event.keyCode == 13 || event.keyCode == undefined){
+                $.ajax({
+                    url : './motores/hanumat.php',
+                    type : 'post',
+                    dataType : 'JSON',
+                    data : {
+                        r : 'l',
+                        usr : $("#usuario").val(),
+                        pwd : $("#pwd").val()
+                    }
+                }).done(function(r) {
+                    if (r.error == '0') {
+                        window.location.href = "./" + r.pagina;
+                    } else {
+                        console.log(r);
+                        toastr.error('Error de credenciales')
+                    }
+                });
+                return false;
+            }
+        }
---
+++ unknown.md
@@ -0,0 +1,5 @@
+<?= NOMBRE_APLICACION ?>
+   <?php
+            "Hola, soy un script colado de PHP";
+        ?> <!-- este script NO se visualiza en el la página web, ya que php trabaja en el servidor -->
+        <?= NOMBRE_APLICACION ?>
